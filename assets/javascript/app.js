cb3fe19b1ddf549f9edc321313244e8863980f1e
Will H <wandrewhouston@gmail.com>
2018-09-22 17:21:10 -0400

    added questions/answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,6 +11,34 @@ $(document).ready(function() {
 
     var timer
 
+    var questions = [
+        {
+            question: "What year was Mickey Mouse created?",
+            choices: ["1920", "1924", "1928", "1932"],
+            answer: 2
+        },
+        {
+            question: "What was the first feature length animated film?",
+            choices: ["Bambi", "Snow White and the Seven Dwarfs", "Dumbo", "Gertie the Dinosaur"],
+            answer: 1
+        },
+        {
+            question: "Which movie won the first Oscar for Best Animated Feature?",
+            choices: ["Shrek", "Snow White and the Seven Dwarfs", "Spirited Away", "101 Dalmations"],
+            answer: 0
+        },
+        {
+            question: "Which cartoon character was the first to appear on a postage stamp?",
+            choices: ["Mickey Mouse", "Gertie the Dinosaur", "Woody the Woodpecker", "Bugs Bunny"],
+            answer: 3
+        },
+        {
+            question: "Which cartoon character was the first to have a statue erected in their honor?",
+            choices: ["Popeye the Sailor", "Mickey Mouse", "Bugs Bunny", "Gertie the Dinosaur"],
+            answer: 0
+        }
+    ]
+
     var countdown = function() {
         time--
         $(".timer").text("Time Remaining: " + time)
